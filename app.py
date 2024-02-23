39f8bf2591c6946aa7741502b357529103488e9b
Ketan Polawar <115727322+Ketanpolawar@users.noreply.github.com>
2024-02-23 22:49:53 +0530

    hjkj

--- app.py
+++ app.py
@@ -9,3 +9,7 @@ image = 'Ketanpolawar/Deep-Learning-Foundations/Banana-Single.jpg'
 image =tf.keras.utils.load_img(image,target_size=(img_height,img_width))
 img_arr = tf.keras.utils.array_to_img(image)
 img_bat=tf.expand_dims(img_arr,0)
+
+predict = model.predict(img_bat)
+score=tf.nn.softmax(predict)
+model.save('Image_classify.keras')
